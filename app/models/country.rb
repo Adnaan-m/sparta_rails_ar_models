520179697a745b96702a59f6d561ae339e20a87f
Tech-A08 <tech-a08@MacBook-Air-3.local>
2018-10-02 23:18:08 +0100

    completed

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,2 +1,18 @@
 class Country < ApplicationRecord
+  has_many :cities
+
+  validates :name, :presence => true
+  validates :name, :format => { :with => /\A[a-zA-Z]+(?: [a-zA-Z]+)?\z/, :message => "Only letters allowed" }
+  validates :name, length: {maximum: 40}
+
+  validates :continent, :presence => true
+  validates :continent, :format => { :with => /\A[a-zA-Z]+(?: [a-zA-Z]+)?\z/, :message => "Only letters allowed" }
+  validates :continent, length: {maximum: 14}
+
+  validates :population, :presence => true
+  validates :population, :numericality => true
+
+  validates :language, :presence => true
+  validates :language, :format => { :with => /\A[a-zA-Z]+(?: [a-zA-Z]+)?\z/, :message => "Only letters allowed" }
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,8 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+  City.create(name: 'London', capcity: true)
+  City.create(name: 'Leeds', capcity: false)
+  City.create(name: 'Lyon', capcity: false)
